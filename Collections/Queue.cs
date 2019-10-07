33ae428715e948e2da12670c40296b2fa657e3f5
unknown <kameswari.m@bizruntime.net>
2019-10-07 22:14:18 +0530

    Collections

---
+++ Collections/Queue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> s = new Queue<string>();
+            s.Enqueue("Kavya");
+            s.Enqueue("vyshu");
+            s.Enqueue("aparna");
+            s.Enqueue("Ratan");
+            s.Enqueue("gayi");
+
+            foreach (string v in s)
+            {
+                Console.WriteLine(v);
+            }
+
+            Console.WriteLine("Peek element: " + s.Peek());
+            Console.WriteLine("Pop: " + s.Dequeue());
+            Console.WriteLine("After Pop, Peek element: " + s.Peek());
+            Console.ReadKey();
+
+        }
+    }
+}
+
---
+++ Collections/SortedDictionary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, string> v = new SortedDictionary<string, string>();
+            v.Add("5", "Sonoo");
+            v.Add("2", "Peter");
+            v.Add("1", "James");
+            v.Add("3", "Ratan");
+            v.Add("4", "Irfan");
+
+            foreach (KeyValuePair<string, string> kv in v)
+            {
+                Console.WriteLine(kv.Key + " " + kv.Value);
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
