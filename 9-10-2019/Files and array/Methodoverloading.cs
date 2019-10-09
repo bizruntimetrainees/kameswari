b064dfd6d0a5e45f9708e6202d4e0f30a8ebca23
unknown <kameswari.m@bizruntime.net>
2019-10-09 19:54:18 +0530

    files

---
+++ 9-10-2019/Files and array/Methodoverloading.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methodoverloading
+{
+    class Cal
+    {
+        public static int add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+    public class TestMemberOverloading
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Cal.add(12, 23));
+            Console.WriteLine(Cal.add(12, 23, 25));
+            Console.ReadKey();
+        }
+    }
+}
