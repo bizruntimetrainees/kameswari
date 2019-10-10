17a746f37b1f3507a458650dce78a3149045cdc4
unknown <kameswari.m@bizruntime.net>
2019-10-10 23:29:03 +0530

    Collections

---
+++ 8-10-2019/Collections/Dictionary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> v = new Dictionary<string, string>();
+            v.Add("1", "Sonoo");
+            v.Add("2", "Peter");
+            v.Add("3", "James");
+            v.Add("4", "Ratan");
+            v.Add("5", "Irfan");
+
+            foreach (KeyValuePair<string, string> kv in v)
+            {
+                Console.WriteLine(kv.Key + " " + kv.Value);
+                Console.ReadKey();
+            }
+        }
+    }
+}
+   
---
+++ 8-10-2019/Collections/Hashset.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashset
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+
+                var names = new HashSet<String>();
+                names.Add("Kavya");
+                names.Add("Ankit");
+                names.Add("gayatri");
+                names.Add("vyshu");
+
+
+                foreach (var name in names)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+    }
+    
