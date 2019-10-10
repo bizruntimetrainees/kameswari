5e20f78511d6ffebd3110b048e8cd7da87083478
unknown <kameswari.m@bizruntime.net>
2019-10-10 19:00:16 +0530

    functions

---
+++ 5-10-19/Functions/Callbyvalu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functioncall
+{
+    class Program
+    {
+        public void show(int val)
+        {
+            val *= val;
+            Console.WriteLine("value inside the show notification" + val);
+        }
+        static void Main(string[] args)
+        {
+            int val = 50;
+            Program program = new Program();
+            Console.WriteLine("before changing" +val);
+            program.show(val);
+            Console.WriteLine("after changing" +val);
+            Console.ReadKey();
+
+        }
+    }
+}
