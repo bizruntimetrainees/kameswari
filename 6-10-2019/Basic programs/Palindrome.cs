fce7d8f5753a29038d523ba20b5610d325aa2fd2
unknown <kameswari.m@bizruntime.net>
2019-10-10 19:07:56 +0530

    Basic programs

---
+++ 6-10-2019/Basic programs/Palindrome.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace CSharpProgram
+{
+    class Program
+    {
+        // Main function, execution entry point of the program  
+        static void Main(string[] args) // string type parameters  
+        {
+            // Command line arguments  
+            Console.WriteLine("Argument length: " + args.Length);
+            Console.WriteLine("Supplied Arguments are:");
+            foreach (Object obj in args)
+            {
+                Console.WriteLine(obj);
+                Console.ReadKey();
+            }
+        }
+    }
+}
