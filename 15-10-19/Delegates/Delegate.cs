7e3b4b93869c99deb75794ec05feb01253a77191
unknown <kameswari.m@bizruntime.net>
2019-10-16 00:07:26 +0530

    Delegates

---
+++ 15-10-19/Delegates/Delegate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        public delegate void Print(int value);
+
+
+        static void Main(string[] args)
+        {
+            PrintHelper(PrintNumber, 10000);
+            PrintHelper(PrintMoney, 10000);
+        }
+
+        public static void PrintHelper(Print delegateFunc, int numToPrint)
+        {
+            delegateFunc(numToPrint);
+        }
+
+        public static void PrintNumber(int num)
+        {
+            Console.WriteLine("Number: {0,-12:N0}", num);
+        }
+
+        public static void PrintMoney(int money)
+        {
+            Console.WriteLine("Money: {0:C}", money);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 15-10-19/Delegates/Delegate3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate2
+{
+    class Program
+    {
+        
+        public delegate void Print(int value);
+
+        static void Main(string[] args)
+        {
+           
+            Print printDel = PrintNumber;
+
+            
+
+            printDel(100000);
+            printDel(200);
+
+         
+            printDel = PrintMoney;
+
+            printDel(10000);
+            printDel(200);
+        }
+
+        public static void PrintNumber(int num)
+        {
+            Console.WriteLine("Number: {0,-12:N0}", num);
+        }
+
+        public static void PrintMoney(int money)
+        {
+            Console.WriteLine("Money: {0:C}", money);
+            Console.ReadKey();
+        }
+    }
+}
