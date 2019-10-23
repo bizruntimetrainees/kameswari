6d3c68b5a346eb3387fd7256647e264932bd274f
unknown <kameswari.m@bizruntime.net>
2019-10-23 21:59:08 +0530

    serializing and deserializing json

---
+++ Json2.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ConsoleApp32
+{
+
+    class SerializeObject
+    {
+        public class Product
+        {
+            public string name { get; set; }
+
+            public DateTime ExpiryDate { get; set; }
+
+            public float price { get; set; }
+
+            public string[] Sizes { get; set; }
+        }
+
+        public static void GetDetail()
+        {
+            Product product = new Product();
+
+            product.name = "Apple";
+            product.ExpiryDate = new DateTime(2019, 10, 23);
+            product.price = 69.35f;
+            product.Sizes = new string[] { "Small", "Medium", "Large" };
+
+            string output = JsonConvert.SerializeObject(product);
+
+
+            Product DeserializedProduct = JsonConvert.DeserializeObject<Product>(output);
+            Console.WriteLine("Name :: " + DeserializedProduct.name);
+            Console.WriteLine("DateTime :: " + DeserializedProduct.ExpiryDate);
+            Console.WriteLine("Price :: " + DeserializedProduct.price);
+            Console.WriteLine("Sizes ::" +JsonConvert.SerializeObject(DeserializedProduct.Sizes));
+}
+        public static void Main(string[] args)
+        {
+            GetDetail();
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ json.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ConsoleApp1
+{
+
+    class SerializeObject
+    {
+        public class Product
+        {
+
+
+            public DateTime ExpiryDate { get; set; }
+        }
+
+        public static void GetDetail()
+        {
+            Product product = new Product();
+            product.ExpiryDate = new DateTime(2008, 12, 28);
+
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.Converters.Add(new JavaScriptDateTimeConverter());
+            serializer.NullValueHandling = NullValueHandling.Ignore;
+
+            using (StreamWriter sw = new StreamWriter("G:\\ram.txt"))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(writer, product);
+
+
+                Console.WriteLine("complete");
+            }
+        }
+        public static void Main(string[] args)
+        {
+            GetDetail();
+            Console.ReadKey();
+        }
+    }
+}
