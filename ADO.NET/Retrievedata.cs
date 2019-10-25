0076db2684d752ae5dc9f4f5c246f7aaeb5b40aa
unknown <kameswari.m@bizruntime.net>
2019-10-25 20:44:18 +0530

    Retrieve the data and Rename the Table

---
+++ ADO.NET/Retrievedata.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ConString = @"Data Source=106.51.126.111;" + "Initial Catalog=kameswari;" + "User id=sa;" +
+ "Password=bizruntime@123;";
+            SqlConnection con = new SqlConnection(ConString);
+            string querystring = "Select * from Customers";
+            con.Open();
+            SqlCommand cmd = new SqlCommand(querystring, con);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader[0].ToString() + " " + reader[1].ToString() + " " + reader[2].ToString()+" "+ reader[3].ToString());
+            }
+        }
+    }
+}
