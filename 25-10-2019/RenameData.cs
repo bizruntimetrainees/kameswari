50d8b1363c554e14cc3111b6e7fce31123575652
unknown <kameswari.m@bizruntime.net>
2019-10-26 21:33:45 +0530

    Create data base and rename database

---
+++ 25-10-2019/RenameData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.SqlClient;
+ 
+namespace RenameDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con = new SqlConnection( @"Data Source=106.51.126.111;" + "Initial Catalog=kameswari;" + "User id=sa;" +
+ "Password=bizruntime@123;");
+            string query = "ALTER DATABASE Customers MODIFY NAME = MobileShop";
+            SqlCommand cmd = new SqlCommand(query, con);
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Database Renamed Successfully");
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error Generated. Details: " + e.ToString());
+            }
+            finally
+            {
+                con.Close();
+                Console.ReadKey();
+            }
+        }
+    }
+}
