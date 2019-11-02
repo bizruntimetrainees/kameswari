65e9b2d9eff223cdc24e8414b1cd476fa901a1ed
kameswari532 <56122071+kameswari532@users.noreply.github.com>
2019-11-02 16:57:15 +0530

    Delete unsubscribe.js

--- 21-10-19/unsubscribe.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var rxjs_1 = require("rxjs");
-var observable = rxjs_1.from([10, 20, 30]);
-var subscription = observable.subscribe(function (x) { return console.log(x); });
-// Later:
-subscription.unsubscribe();
