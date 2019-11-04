cc498bd6f2d0a716ab96065c15c06023de4f9042
kameswari532 <56122071+kameswari532@users.noreply.github.com>
2019-11-04 07:13:40 +0530

    Update ValuesController.cs

--- 29-10-19/WebApplication9/Controllers/ValuesController.cs
+++ 29-10-19/WebApplication9/Controllers/ValuesController.cs
@@ -9,7 +9,7 @@ namespace WebApplication9.Controllers
 {
     public class ValuesController : ApiController
     {
-        // GET api/values
+        
         static List<string> strings = new List<String>()
         {
          "value0","Value1","value2"
@@ -19,25 +19,25 @@ namespace WebApplication9.Controllers
         return strings;
         }
 
-        // GET api/values/5
+       
         public string Get(int id)
         {
             return strings[id];
         }
 
-        // POST api/values
+        
         public void Post([FromBody]string value)
         {
             strings.Add(value);
         }
 
-        // PUT api/values/5
+        
         public void Put(int id, [FromBody]string value)
         {
             strings[id] = value;
         }
 
-        // DELETE api/values/5
+      
         public void Delete(int id)
         {
             strings.RemoveAt(id);
